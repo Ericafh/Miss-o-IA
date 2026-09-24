8b7119be8fcc22b51bdde0722fe3dbe5cde4e3ac
Ericafh <h.erica@escola.pr.gov.br>
2026-09-24 11:46:20 -0300

    ..

--- js/script.js
+++ js/script.js
@@ -34,11 +34,6 @@ function respostaSelecionada(opcaoSelecionada) {
     mostraPergunta();
 }
 
-function aleatorio(lista) {
-const posicao = Math.floor(Math.random()* lista.length);
-return lista[posicao];
-}
-
 function mostraResultado(){
     caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
     textoResultado.textContent = historiaFinal;
